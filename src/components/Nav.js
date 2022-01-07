81bced262d3745a38c5be9ea11fabdaa1fdd81c6
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-01-06 21:13:14 -0800

    update layout styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,6 +13,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
 
   li {
@@ -39,6 +40,9 @@ const NavStyles = styled.nav`
     &:hover {
       color: var(--red);
     }
+    ${'' /* &[aria-current='page'] {
+      color: var(--red);
+    } */}
   }
 `;
 
